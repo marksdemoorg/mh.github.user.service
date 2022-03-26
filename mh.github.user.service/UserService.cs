e8767c2dac5e4d2cead91bd7da8d6c875cae8e99
Mark <marky72mark@outlook.com>
2022-03-26 10:25:45 -0700

    bad commit

--- mh.github.user.service/UserService.cs
+++ mh.github.user.service/UserService.cs
@@ -1,11 +1,16 @@
 ﻿using mh.github.user.entity;
 using mh.github.user.interfaces.appservice;
 using mh.github.user.interfaces.Data;
+using System.Runtime.InteropServices;
 
 namespace mh.github.user.service
 {
 	public class UserService : IUserService
 	{
+
+		[DllImport("User32.dll")]
+		private static extern int MyVar();
+
 		private readonly IUserRepository userRepository;
 
 		public UserService(IUserRepository userRepository)
